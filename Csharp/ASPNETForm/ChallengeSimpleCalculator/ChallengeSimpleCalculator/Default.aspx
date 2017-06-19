f8357ba0e51d2459416e547c79064589946dd306
John Vicencio <jvicencio@johnvicencio.com>
2017-06-18 17:46:28 -0700

    Added ChallengeSimpleCalculator web app

---
+++ Csharp/ASPNETForm/ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <h1>Simple Calculator</h1>
+        <span class="auto-style1">First Value:</span>
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        <br />
+        <span class="auto-style1">Second Value:</span>
+        <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="plusButton" runat="server" OnClick="plusButton_Click" Text="+" />
+&nbsp;<asp:Button ID="subtractButton" runat="server" OnClick="subtractButton_Click" Text="-" />
+&nbsp;<asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" />
+&nbsp;<asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" Text="/" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server" BackColor="#99CCFF" Font-Bold="True" Font-Size="Larger"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Csharp/ASPNETForm/ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first + second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first - second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first * second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(firstTextBox.Text);
+            double second = double.Parse(secondTextBox.Text);
+            double result = first / second;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
