59fd5df9990c51f4f93930668e5bda314b182599
John Vicencio <jvicencio@johnvicencio.com>
2017-06-19 20:31:36 -0700

    Readded ChallengePapaBobsWebsite

---
+++ Csharp/ASPNETWebForms/ChallengeFirstPapaBobsWebsite/ChallengeFirstPapaBobsWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengeFirstPapaBobsWebsite.Startup))]
+namespace ChallengeFirstPapaBobsWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
