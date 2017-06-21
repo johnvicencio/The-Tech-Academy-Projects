0b2b9c2eee35f60ebba1a560d6f2e37d6daca71a
John Vicencio <jvicencio@johnvicencio.com>
2017-06-20 22:33:59 -0700

    Added ChallengeForXmenBattleCount

---
+++ Csharp/ASPNETWebForms/ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+            int largestNumberIndex = 0;
+            int smallestNumberIndex = 0;
+
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                // Is this the largest count so far?
+                if (numbers[i] > numbers[largestNumberIndex])
+                {
+                    largestNumberIndex = i;
+                }
+
+                // Is this the smallest count so far?
+                if (numbers[i] < numbers[smallestNumberIndex])
+                {
+                    smallestNumberIndex = i;
+                }
+
+            }
+
+            result = String.Format("Most battles belong to: {0} (Value: {1})", names[largestNumberIndex], numbers[largestNumberIndex]);
+            result += String.Format("<br />Least battles belong to: {0} (Value: {1})", names[smallestNumberIndex], numbers[smallestNumberIndex]);
+
+            resultLabel.Text = result;
+        }
+    }
+}
