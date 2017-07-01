0fb3c9f84e8b084db557a55bae9a8e5b4c146e35
John Vicencio <jvicencio@johnvicencio.com>
2017-06-30 21:24:18 -0700

    Added ChallengeHeroMOnster Part 1

---
+++ Csharp/ASPNETWebForms/ChallengeHeroMonsterClassesPart1/ChallengeHeroMonsterClassesPart1/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.Health = 35;
+            hero.DamageMaximum = 20;
+            hero.AttackBonus = false;
+
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.Health = 21;
+            monster.DamageMaximum = 25;
+            monster.AttackBonus = true;
+
+            int damage = hero.Attack();
+            monster.Defend(damage);
+
+            damage = monster.Attack();
+            hero.Defend(damage);
+
+
+            printStats(hero);
+            printStats(monster);
+
+        }
+
+        private void printStats(Character character)
+        {
+            resultLabel.Text += String.Format("<p>Name: {0} - Health: {1} - DamageMaximum: {2} - AttackBonus: {3}</p>",
+                character.Name,
+                character.Health,
+                character.DamageMaximum.ToString(),
+                character.AttackBonus.ToString());
+        }
+
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public int Attack()
+        {
+            Random random = new Random();
+            int damage = random.Next(this.DamageMaximum);
+            return damage;
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health -= damage;
+        }
+    }
+
+}
