25a4ab2c8f9c9453cd76125c19674023bbff68bd
John Vicencio <jvicencio@johnvicencio.com>
2017-07-01 10:23:22 -0700

    Added Part 2 of Hero Monster Challenge

---
+++ Csharp/ASPNETWebForms/ChallengeHeroMonsterClassesPart2/ChallengeHeroMonsterClassesPart2/Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClassesPart2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.Health = 35;
+            hero.DamageMaximum = 20;
+            hero.AttackBonus = false;
+
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.Health = 21;
+            monster.DamageMaximum = 25;
+            monster.AttackBonus = true;
+
+            Dice dice = new Dice();
+
+            // Bonus
+            if (hero.AttackBonus)
+                monster.Defend(hero.Attack(dice));
+            if (monster.AttackBonus)
+                hero.Defend(monster.Attack(dice));
+
+            while (hero.Health > 0 && monster.Health > 0)
+            {
+                monster.Defend(hero.Attack(dice));
+                hero.Defend(monster.Attack(dice));
+
+
+                printStats(hero);
+                printStats(monster);
+
+            }
+
+            displayResult(hero, monster);
+
+        }
+
+        private void displayResult(Character opponent1, Character opponent2)
+        {
+            if (opponent1.Health <= 0 && opponent2.Health <= 0)
+                resultLabel.Text += String.Format("<p>Both {0} and {1} died.", opponent1.Name, opponent2.Name);
+            else if (opponent1.Health <= 0)
+                resultLabel.Text += String.Format("<p>{0} defeats {1}</p>", opponent2.Name, opponent1.Name);
+            else
+                resultLabel.Text += String.Format("<p>{0} defeats {1}</p>", opponent1.Name, opponent2.Name);
+        }
+
+        private void printStats(Character character)
+        {
+            resultLabel.Text += String.Format("<p>Name: {0} - Health: {1} - DamageMaximum: {2} - AttackBonus: {3}</p>",
+                character.Name,
+                character.Health,
+                character.DamageMaximum.ToString(),
+                character.AttackBonus.ToString());
+        }
+
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public int Attack(Dice dice)
+        {
+            //Random random = new Random();
+            //int damage = random.Next(this.DamageMaximum);
+
+            dice.Sides = this.DamageMaximum;
+            return dice.Roll();
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health -= damage;
+        }
+    }
+
+    class Dice
+    {
+        public int Sides { get; set; }
+
+        Random random = new Random();
+        public int Roll()
+        {
+            return random.Next(this.Sides);
+        }
+    }
+
+}
