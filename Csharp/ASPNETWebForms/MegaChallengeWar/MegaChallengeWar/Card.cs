92e8e59b772dd016dc676dd040c06dde757ce11d
John Vicencio <jvicencio@johnvicencio.com>
2017-07-21 20:57:46 -0700

    ChallengeWar solution 2nd part

--- Csharp/ASPNETWebForms/MegaChallengeWar/MegaChallengeWar/Card.cs
+++ Csharp/ASPNETWebForms/MegaChallengeWar/MegaChallengeWar/Card.cs
@@ -9,5 +9,30 @@ namespace MegaChallengeWar
     {
         public string Suit { get; set; }
         public string Kind { get; set; }
+
+        public int CardValue()
+        {
+            int value = 0;
+
+            switch (this.Kind)
+            {
+                case "Jack":
+                    value = 11;
+                    break;
+                case "Queen":
+                    value = 12;
+                    break;
+                case "King":
+                    value = 13;
+                    break;
+                case "Ace":
+                    value = 14;
+                    break;
+                default:
+                    value = int.Parse(this.Kind);
+                    break;
+            }
+            return value;
+        }
     }
 }
--- Csharp/ASPNETWebForms/MegaChallengeWar/MegaChallengeWar/Game.cs
+++ Csharp/ASPNETWebForms/MegaChallengeWar/MegaChallengeWar/Game.cs
@@ -9,18 +9,69 @@ namespace MegaChallengeWar
     {
         private Player _player1;
         private Player _player2;
+        private List<Card> _bounty;
 
         public Game(string player1Name, string player2Name)
         {
             _player1 = new Player() { Name = player1Name };
             _player2 = new Player() { Name = player2Name };
+            _bounty = new List<Card>();
         }
 
         public string Play()
         {
             Deck deck = new Deck();
-            return deck.Deal(_player1, _player2);
+            string result = deck.Deal(_player1, _player2);
 
+            //return deck.Deal(_player1, _player2);
+            int round = 0;
+            while (_player1.Cards.Count != 0 && _player2.Cards.Count != 0)
+            {
+                Card player1Card = getCard(_player1);
+                Card player2Card = getCard(_player2);
+
+                performEvaluation(_player1, _player2, player1Card, player2Card);
+
+                round++;
+                if (round > 20)
+                    break;
+
+
+            }
+            // Determine the winner
+            result += determineWinner();
+            return result;
+
+        }
+
+        private Card getCard(Player player)
+        {
+            Card card = player.Cards.ElementAt(0);
+            player.Cards.Remove(card);
+            _bounty.Add(card);
+            return card;
+        }
+
+        private void performEvaluation(Player player1, Player player2, Card card1, Card card2)
+        {
+            if (card1.CardValue() > card2.CardValue())
+                player1.Cards.AddRange(_bounty);
+            else
+                player2.Cards.AddRange(_bounty);
+            _bounty.Clear();
+        }
+
+
+        private string determineWinner()
+        {
+            string result = "";
+            if (_player1.Cards.Count > _player2.Cards.Count)
+                result += "<br/>Player 1 wins";
+            else
+                result += "<br/>Player 2 wins";
+
+            result += "<br/>Player 1:" + _player1.Cards.Count + " Player2:" + _player2.Cards.Count;
+            return result;
         }
 
 
